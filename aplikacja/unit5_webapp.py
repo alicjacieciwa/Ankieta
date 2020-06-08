b1da7bb03427e2dc91d580e161d0ea4c09472563
paulib9 <paulina.x.balon@gmail.com>
2020-06-08 22:24:59 +0200

    form3

--- aplikacja/unit5_webapp.py
+++ aplikacja/unit5_webapp.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import statistics
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///formdata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
 
@@ -16,18 +16,72 @@ class Formdata(db.Model):
     plec = db.Column(db.String)
     wiek = db.Column(db.Integer)
     wyksztalcenie = db.Column(db.String)
-    q1 = db.Column(db.String)
-    q2 = db.Column(db.String)
-    q3 = db.Column(db.String)
+    q1_1 = db.Column(db.String)
+    q2_1 = db.Column(db.String)
+    q3_1 = db.Column(db.String)
+    q1_2 = db.Column(db.String)
+    q2_2 = db.Column(db.String)
+    q3_2 = db.Column(db.String)
+    q1_3 = db.Column(db.String)
+    q2_3 = db.Column(db.String)
+    q3_3 = db.Column(db.String)
+    q1_4 = db.Column(db.String)
+    q2_4 = db.Column(db.String)
+    q3_4 = db.Column(db.String)
+    q1_5 = db.Column(db.String)
+    q2_5 = db.Column(db.String)
+    q3_5 = db.Column(db.String)
+    q1_6 = db.Column(db.String)
+    q2_6 = db.Column(db.String)
+    q3_6 = db.Column(db.String)
+    q1_7 = db.Column(db.String)
+    q2_7 = db.Column(db.String)
+    q3_7 = db.Column(db.String)
+    q1_8 = db.Column(db.String)
+    q2_8 = db.Column(db.String)
+    q3_8 = db.Column(db.String)
+    q1_9 = db.Column(db.String)
+    q2_9 = db.Column(db.String)
+    q3_9 = db.Column(db.String)
+    q1_10 = db.Column(db.String)
+    q2_10 = db.Column(db.String)
+    q3_10 = db.Column(db.String)
 
-
-    def __init__(self, plec, wiek, wyksztalcenie, q1, q2, q3):
+    def __init__(self, plec, wiek, wyksztalcenie, q1_1, q2_1, q3_1, q1_2, q2_2, q3_2, q1_3, q2_3, q3_3, q1_4, q2_4, q3_4, q1_5, q2_5, q3_5, q1_6, q2_6, q3_6, q1_7, q2_7, q3_7, q1_8, q2_8, q3_8, q1_9, q2_9, q3_9, q1_10, q2_10, q3_10):
         self.plec = plec
         self.wiek = wiek
         self.wyksztalcenie = wyksztalcenie
-        self.q1 = q1
-        self.q2 = q2
-        self.q3 = q3
+        self.q1_1 = q1_1
+        self.q2_1 = q2_1
+        self.q3_1 = q3_1
+        self.q1_2 = q1_2
+        self.q2_2 = q2_2
+        self.q3_2 = q3_2
+        self.q1_3 = q1_3
+        self.q2_3 = q2_3
+        self.q3_3 = q3_3
+        self.q1_4 = q1_4
+        self.q2_4 = q2_4
+        self.q3_4 = q3_4
+        self.q1_5 = q1_5
+        self.q2_5 = q2_5
+        self.q3_5 = q3_5
+        self.q1_6 = q1_6
+        self.q2_6 = q2_6
+        self.q3_6 = q3_6
+        self.q1_7 = q1_7
+        self.q2_7 = q2_7
+        self.q3_7 = q3_7
+        self.q1_8 = q1_8
+        self.q2_8 = q2_8
+        self.q3_8 = q3_8
+        self.q1_9 = q1_9
+        self.q2_9 = q2_9
+        self.q3_9 = q3_9
+        self.q1_10 = q1_10
+        self.q2_10 = q2_10
+        self.q3_10 = q3_10
+
 
 db.create_all()
 
@@ -68,14 +122,43 @@ def show_result():
 
     # Some simple statistics for sample questions
 
-    q1 = []
-    q2 = []
-    q3 = []
+    q1_1 = []
+    q2_1 = []
+    q3_1 = []
+    q1_2 = []
+    q2_2 = []
+    q3_2 = []
+    q1_3 = []
+    q2_3 = []
+    q3_3 = []
+    q1_4 = []
+    q2_4 = []
+    q3_4 = []
+    q1_5 = []
+    q2_5 = []
+    q3_5 = []
+    q1_6 = []
+    q2_6 = []
+    q3_6 = []
+    q1_7 = []
+    q2_7 = []
+    q3_7 = []
+    q1_8 = []
+    q2_8 = []
+    q3_8 = []
+    q1_9 = []
+    q2_9 = []
+    q3_9 = []
+    q1_10 = []
+    q2_10 = []
+    q3_10 = []
 
-    for el in fd_list:
-        q1.append(int(el.q1))
-        q2.append(int(el.q2))
-        q3.append(int(el.q3))
+    #
+    #
+    # for el in fd_list:
+    #     q1_1.append(int(el.q1_1))
+    #     q2_1.append(int(el.q2_1))
+    #     q3_1.append(int(el.q3_1))
 
     # if len(q1) > 0:
     #     mean_q1 = statistics.mean(q1)
@@ -99,12 +182,41 @@ def save():
     plec = request.form['plec']
     wiek = request.form['wiek']
     wyksztalcenie = request.form['wyksztalcenie']
-    q1 = request.form['q1']
-    q2 = request.form['q2']
-    q3 = request.form['q3']
+    q1_1 = request.form['q1_1']
+    q2_1 = request.form['q2_1']
+    q3_1 = request.form['q3_1']
+    q1_2 = request.form['q1_2']
+    q2_2 = request.form['q2_2']
+    q3_2 = request.form['q3_2']
+    q1_3 = request.form['q1_3']
+    q2_3 = request.form['q2_3']
+    q3_3 = request.form['q3_3']
+    q1_4 = request.form['q1_4']
+    q2_4 = request.form['q2_4']
+    q3_4 = request.form['q3_4']
+    q1_5 = request.form['q1_5']
+    q2_5 = request.form['q2_5']
+    q3_5 = request.form['q3_5']
+    q1_6 = request.form['q1_6']
+    q2_6 = request.form['q2_6']
+    q3_6 = request.form['q3_6']
+    q1_7 = request.form['q1_7']
+    q2_7 = request.form['q2_7']
+    q3_7 = request.form['q3_7']
+    q1_8 = request.form['q1_8']
+    q2_8 = request.form['q2_8']
+    q3_8 = request.form['q3_8']
+    q1_9 = request.form['q1_9']
+    q2_9 = request.form['q2_9']
+    q3_9 = request.form['q3_9']
+    q1_10 = request.form['q1_10']
+    q2_10 = request.form['q2_10']
+    q3_10 = request.form['q3_10']
+
+
 
     # Save the data
-    fd = Formdata(plec, wiek, wyksztalcenie, q1, q2, q3)
+    fd = Formdata(plec, wiek, wyksztalcenie,q1_1, q2_1, q3_1, q1_2, q2_2, q3_2, q1_3, q2_3, q3_3, q1_4, q2_4, q3_4, q1_5, q2_5, q3_5, q1_6, q2_6, q3_6, q1_7, q2_7, q3_7, q1_8, q2_8, q3_8, q1_9, q2_9, q3_9, q1_10, q2_10, q3_10)
     db.session.add(fd)
     db.session.commit()
 
